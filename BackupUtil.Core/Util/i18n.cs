b5b219bb3eabc1f99271dbd8b8cfd99e6c021af9
xX-Miyu-Xx <laura.giese@viacesi.fr>
2025-05-07 12:03:42 +0200

    Implemented i18n util and used it in BackupTransactionBuilder.cs

---
+++ BackupUtil.Core/Util/i18n.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace BackupUtil.Core.Util;
+
+public static class I18N
+{
+    private static readonly ResourceManager ResourceManager =
+        new ResourceManager("BackupUtil.Core.Resources.Strings", typeof(I18N).Assembly);
+
+    public static string GetLocalizedMessage(string key)
+    {
+        return ResourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? key;
+    }
+}
