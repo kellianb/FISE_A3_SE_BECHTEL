771e5b6595cca00c59e9ffa8fb0b6c8db861320a
kellian <kellian.bechtel@proton.me>
2025-05-07 14:19:26 +0200

    Fix i18n RessourceManager naming

--- BackupUtil.Core/Util/i18n.cs
+++ BackupUtil.Core/Util/i18n.cs
@@ -6,11 +6,11 @@ namespace BackupUtil.Core.Util;
 
 public static class I18N
 {
-    private static readonly ResourceManager ResourceManager =
+    private static readonly ResourceManager s_resourceManager =
         new ResourceManager("BackupUtil.Core.Resources.Strings", typeof(I18N).Assembly);
 
     public static string GetLocalizedMessage(string key)
     {
-        return ResourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? key;
+        return s_resourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? key;
     }
 }
