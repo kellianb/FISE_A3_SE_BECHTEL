3b0e3046cfed98720aa86ab3265bfdd3f480cc9e
Kellian Bechtel <109019244+kellianb@users.noreply.github.com>
2025-05-07 16:56:22 +0200

    Implemented CLI, fixed I18N (#18)
    Implemented basic CLI
    Renamed I18N string folder to match path in I18N util

---
+++ BackupUtil.Cli/BackupUtilCli.cs
@@ -0,0 +1,62 @@
+using System.CommandLine;
+using System.Text.Json;
+using BackupUtil.Core.Executor;
+using BackupUtil.Core.Job;
+using BackupUtil.Core.Transaction;
+
+namespace BackupUtil.Cli;
+
+internal class BackupUtilCli
+{
+    private readonly RootCommand _command;
+
+    public BackupUtilCli()
+    {
+        Argument<string> sourcePath = new("source-path", "Source path of the backup");
+        Argument<string> targetPath = new("target-path", "Target path of the backup");
+        Option<bool> recursive = new("--recursive", "Make the backup recursive");
+        Option<bool> differential = new("--differential", "Make the backup differential");
+
+        _command = [sourcePath, targetPath, recursive, differential];
+
+        _command.SetHandler(CommandHandler,
+            sourcePath,
+            targetPath,
+            recursive,
+            differential
+        );
+    }
+
+    public int Invoke(string[] args)
+    {
+        return _command.Invoke(args);
+    }
+
+    private static void CommandHandler(string sourcePath, string targetPath, bool recursive, bool differential)
+    {
+        Job job = new(sourcePath, targetPath, recursive, differential);
+
+        try
+        {
+            BackupTransaction transaction = BackupTransactionBuilder.Build(job);
+
+            Console.WriteLine("Making these changes");
+            Console.WriteLine(JsonSerializer.Serialize(transaction, new JsonSerializerOptions { WriteIndented = true }));
+            Console.WriteLine("Is this ok [Y/n]");
+
+            if (new List<string>(["Y", "y", ""]).Contains(Console.ReadLine() ?? "n"))
+            {
+                BackupTransactionExecutor.Execute(transaction);
+                Console.WriteLine("Done");
+            }
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return;
+        }
+
+    }
+}
---
+++ BackupUtil.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace BackupUtil.Cli;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr-FR");
+        return new BackupUtilCli().Invoke(args);
+    }
+}
