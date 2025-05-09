5237051dd96923558da3291730124a4becdcf882
kellian <kellian.bechtel@proton.me>
2025-05-09 09:17:04 +0200

    Added more methods to the I18n class, removed explicit language setting in CLI

--- BackupUtil.I18n/I18n.cs
+++ BackupUtil.I18n/I18n.cs
@@ -5,11 +5,31 @@ namespace BackupUtil.I18n;
 
 public static class I18N
 {
+    private static readonly List<CultureInfo> s_supportedCultures =
+    [
+        new("fr-FR"), new("en-GB")
+    ];
+
     private static readonly ResourceManager s_resourceManager =
-        new ResourceManager("BackupUtil.I18n.Resources.Strings", typeof(I18N).Assembly);
+        new("BackupUtil.I18n.Resources.Strings", typeof(I18N).Assembly);
 
     public static string GetLocalizedMessage(string key)
     {
         return s_resourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? key;
+    }
+
+    public static void SetCulture(CultureInfo culture)
+    {
+        Thread.CurrentThread.CurrentUICulture = culture;
+    }
+
+    public static CultureInfo GetCurrentCulture()
+    {
+        return Thread.CurrentThread.CurrentUICulture;
+    }
+
+    public static List<CultureInfo> GetSupportedCultures()
+    {
+        return s_supportedCultures;
     }
 }
