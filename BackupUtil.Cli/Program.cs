067a939f0a989cd51c9ce257f7b155c44f8f6e40
Kellian Bechtel <109019244+kellianb@users.noreply.github.com>
2025-05-09 09:28:46 +0200

    Move I18n to separate project (#19)
    ---------
    
    Co-authored-by: xX-Miyu-Xx <laura.giese@viacesi.fr>

--- BackupUtil.Cli/Program.cs
+++ BackupUtil.Cli/Program.cs
@@ -1,12 +1,9 @@
-﻿using System.Globalization;
-
-namespace BackupUtil.Cli;
+﻿namespace BackupUtil.Cli;
 
 internal class Program
 {
     private static int Main(string[] args)
     {
-        Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr-FR");
         return new BackupUtilCli().Invoke(args);
     }
 }
