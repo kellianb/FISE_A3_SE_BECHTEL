81c65d2fa81c8c8884e91822f327bc0f61b2f592
kellian <kellian.bechtel@proton.me>
2025-05-13 22:29:44 +0200

    Added logging WIP

--- BackupUtil.Core/Util/Config.cs
+++ BackupUtil.Core/Util/Config.cs
@@ -1,10 +1,12 @@
 namespace BackupUtil.Core.Util;
 
-public class Config
+internal class Config
 {
     // Default file path to look for a job file
     public static string DefaultJobFilePath { get; } = "BackupJobs.json";
 
     // Default max job count for the job manager
     public static uint DefaultMaxJobCount { get; } = 5;
+
+    public static string LoggingDirectory { get; } = "/tmp/EasySave";
 }
