6f016ac1143ce034a819d082a3a30b0a038b124e
kellian <kellian.bechtel@proton.me>
2025-05-13 16:14:19 +0200

    Changed jobFilePath to FileSystemInfo instead of string

--- BackupUtil.Cli/Command/CreateJobCommand.cs
+++ BackupUtil.Cli/Command/CreateJobCommand.cs
@@ -13,7 +13,7 @@ internal static class CreateJobCommand
         Option<string> name = new(["--name", "-n"], "Name of the job");
         Option<bool> recursive = new(["--recursive", "-r"], "Make the backup recursive");
         Option<bool> differential = new(["--differential", "-d"], "Make the backup differential.");
-        Option<string> jobFilePath = new(["--job-file-path", "-o"], "File path to save the job to");
+        Option<FileSystemInfo> jobFilePath = new(["--job-file-path", "-o"], "File path to save the job to");
 
         System.CommandLine.Command command = new("create", "Create a backup job");
 
@@ -37,7 +37,7 @@ internal static class CreateJobCommand
     }
 
     private static void CommandHandler(FileSystemInfo sourcePath, FileSystemInfo targetPath, bool recursive,
-        bool differential, string? name = null, string? jobFilePath = null)
+        bool differential, string? name = null, FileSystemInfo? jobFilePath = null)
     {
         Job job = new(sourcePath.FullName, targetPath.FullName, recursive, differential, name);
 
@@ -47,16 +47,16 @@ internal static class CreateJobCommand
 
             try
             {
-                manager.AddJobsFromFile(jobFilePath);
+                manager.AddJobsFromFile(jobFilePath?.FullName);
             }
             catch (Exception e)
             {
-                // Only catch the exception if the default job file path was specified
+                // Only catch the exception if the default job file path was used
                 if (jobFilePath is not null) throw;
             }
 
             manager.AddJob(job)
-                .ExportAll(jobFilePath);
+                .ExportAll(jobFilePath?.FullName);
         }
         catch (Exception e)
         {
