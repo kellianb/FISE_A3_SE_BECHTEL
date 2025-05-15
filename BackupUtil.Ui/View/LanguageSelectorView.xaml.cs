bf29252a281177a4ccef4071484ef25a6ed33375
xX-Miyu-Xx <laura.giese@viacesi.fr>
2025-05-15 12:24:56 +0200

    Added language selector view and viewmodel, and added it to JobList View (WIP)

---
+++ BackupUtil.Ui/View/LanguageSelectorView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using BackupUtil.I18n;
+using BackupUtil.ViewModel;
+
+namespace BackupUtil.Ui.View;
+
+public partial class LanguageSelectorView : UserControl
+{
+    public LanguageSelectorView()
+    {
+        InitializeComponent();
+        DataContext = new LanguageSelectorViewModel();
+    }
+}
+
