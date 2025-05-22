a24ab0f800060de7da6051f818e91ed18ad66072
xX-Miyu-Xx <laura.giese@viacesi.fr>
2025-05-22 10:58:45 +0200

    added new window for new job configuration (WIP)

---
+++ BackupUtil.Ui/View/ConfigureJobWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace BackupUtil.Ui.View;
+
+public partial class ConfigureJobWindow : Window
+{
+    public ConfigureJobWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void SendJobData(object sender, RoutedEventArgs routedEventArgs)
+    {
+
+    }
+}
+
