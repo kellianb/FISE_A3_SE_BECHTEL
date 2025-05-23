a05e044e4d70893650e0fb8420437868a18e48d1
xX-Miyu-Xx <laura.giese@viacesi.fr>
2025-05-23 15:50:47 +0200

    added select encryption type (WIP)

---
+++ BackupUtil.Ui/View/CreateJobView.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using BackupUtil.Crypto;
+using BackupUtil.I18n;
+using BackupUtil.ViewModel.ViewModel;
+using Microsoft.Win32;
+
+namespace BackupUtil.Ui.View;
+
+public partial class CreateJobView : Window
+{
+    private FileSystemInfo _sourcePath;
+    private FileSystemInfo _targetPath;
+    private readonly JobListingViewModel _jobListingViewModel;
+
+    public CreateJobView(JobListingViewModel jobListingViewModel)
+    {
+        InitializeComponent();
+        this._jobListingViewModel = jobListingViewModel;
+        DataContext = jobListingViewModel;
+        encryptionTypes = enum EncryptionType
+    }
+
+    public void SelectSourcePath(object sender, RoutedEventArgs routedEventArgs)
+    {
+        OpenFolderDialog dialog = new();
+
+        // Show the dialog
+        if (dialog.ShowDialog() == true)
+        {
+            if (Directory.Exists(dialog.FolderName))
+            {
+                _sourcePath = new DirectoryInfo(dialog.FolderName);
+            }
+            else
+            {
+                Console.WriteLine(I18N.GetLocalizedMessage("invalidPath"));
+            }
+        }
+    }
+
+    public void SelectTargetPath(object sender, RoutedEventArgs routedEventArgs)
+    {
+        OpenFolderDialog dialog = new();
+
+        // Show the dialog
+        if (dialog.ShowDialog() == true)
+        {
+            if (Directory.Exists(dialog.FolderName))
+            {
+                _targetPath = new DirectoryInfo(dialog.FolderName);
+            }
+            else
+            {
+                Console.WriteLine(I18N.GetLocalizedMessage("invalidPath"));
+            }
+        }
+    }
+
+    public void SendJobData(object sender, RoutedEventArgs routedEventArgs)
+    {
+        //TODO: mandatory fields + add encryptor type (not mandatory) and encryptor key (mandatory if type is not null) + refresh job list on send
+        _jobListingViewModel.AddJob(
+            _sourcePath,
+            _targetPath,
+            RecursiveInput.IsChecked ?? false,
+            DifferentialInput.IsChecked ?? false,
+            NameInput.Text
+        );
+
+        Close();
+    }
+
+    public void SelectEncryptionType(object sender, RoutedEventArgs routedEventArgs)
+    {
+            string? selectedType = EncryptionTypeInput.SelectedItem.ToString();
+            TextBlock encryptionKeyLabel = new()
+            {
+                Name = "EncryptionKeyLabel",
+                Text = I18N.GetLocalizedMessage("encryptionKeyLabel"),
+                Margin= new Thickness(0, 0, 0, 5)
+            };
+            TextBox encryptionKeyInput = new()
+            {
+                Name = "EncryptionKeyInput",
+                HorizontalAlignment= HorizontalAlignment.Stretch
+            };
+            if (selectedType != null && selectedType != I18N.GetLocalizedMessage("noEncryption") && !EncryptionPanel.Children.Contains(encryptionKeyInput))
+            {
+                EncryptionPanel.Children.Add(encryptionKeyLabel);
+                EncryptionPanel.Children.Add(encryptionKeyInput);
+            }
+            else if (selectedType == I18N.GetLocalizedMessage("noEncryption") && EncryptionPanel.Children.Contains(encryptionKeyInput))
+            {
+                EncryptionPanel.Children.Remove(encryptionKeyInput);
+                EncryptionPanel.Children.Remove(encryptionKeyLabel);
+            }
+    }
+}
