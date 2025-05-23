73ebd5f1895f9fe927a9884d46295ca05fe32118
kellian <kellian.bechtel@proton.me>
2025-05-23 10:44:34 +0200

    Fixed JSON serialization, limited GitHub actions build to Cli

--- BackupUtil.Core/Util/JsonSerialization.cs
+++ BackupUtil.Core/Util/JsonSerialization.cs
@@ -1,9 +1,11 @@
 using System.Text.Json.Serialization;
 using BackupUtil.Core.Transaction;
+using BackupUtil.Core.Transaction.FileMask;
 
 namespace BackupUtil.Core.Util;
 
 [JsonSerializable(typeof(Job.Job))]
+[JsonSerializable(typeof(FileMask))]
 [JsonSerializable(typeof(List<Job.Job>))]
 [JsonSerializable(typeof(BackupTransaction))]
 internal partial class JsonBackupUtilSerializerContext : JsonSerializerContext
