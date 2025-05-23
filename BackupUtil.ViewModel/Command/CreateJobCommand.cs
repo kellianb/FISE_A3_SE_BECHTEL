bbc8ef4e3b699ec0fc8d0d03eb55458ca1cecc15
kellian <kellian.bechtel@proton.me>
2025-05-23 19:03:39 +0200

    Added file mask validation, worked on the job creation viewmodel, added Commandbase

--- BackupUtil.ViewModel/Command/CreateJobCommand.cs
+++ BackupUtil.ViewModel/Command/CreateJobCommand.cs
@@ -1,12 +1,43 @@
+using System.ComponentModel;
 using BackupUtil.Core.Job;
+using BackupUtil.ViewModel.ViewModel;
 
 namespace BackupUtil.ViewModel.Command;
 
-public class CreateJobCommand(JobManager jobManager) : CommandBase
+public class CreateJobCommand : AsyncCommandBase
 {
-    private readonly JobManager _jobManager = jobManager;
+    private readonly JobManager _jobManager;
+    private readonly JobCreationViewModel _jobCreationViewModel;
+
+    public CreateJobCommand(JobManager jobManager, JobCreationViewModel jobCreationViewModel)
+    {
+        _jobManager = jobManager;
+        _jobCreationViewModel = jobCreationViewModel;
+        _jobCreationViewModel.PropertyChanged += OnViewModelPropertyChanged;
+    }
 
     public override void Execute(object? parameter)
     {
+    }
+
+    public override Task ExecuteAsync(object? parameter)
+    {
+        Job job = new("", "", false, false);
+
+        try
+        {
+            _jobManager.AddJob(job);
+        }
+        catch {}
+        
+        return Task.CompletedTask;
+    }
+
+    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if(e.PropertyName == nameof(_jobCreationViewModel.CanCreateJob))
+        {
+            OnCanExecuteChanged();
+        }
     }
 }
