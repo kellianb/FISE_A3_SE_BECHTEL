01c07778d4141f1decf21ecd6588b5306d8b043c
xX-Miyu-Xx <laura.giese@viacesi.fr>
2025-05-23 11:18:41 +0200

    added all view elements and method of configure job window

--- BackupUtil.Ui/View/ConfigureJobWindow.xaml.cs
+++ BackupUtil.Ui/View/ConfigureJobWindow.xaml.cs
@@ -1,17 +1,71 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using BackupUtil.ViewModel;
+using BackupUtil.I18n;
+using Microsoft.Win32;
 
 namespace BackupUtil.Ui.View;
 
 public partial class ConfigureJobWindow : Window
 {
-    public ConfigureJobWindow()
+    private FileSystemInfo _sourcePath;
+    private FileSystemInfo _targetPath;
+    private JobListViewModel jobListViewModel;
+
+    public ConfigureJobWindow(JobListViewModel jobListViewModel)
     {
         InitializeComponent();
+        this.jobListViewModel = jobListViewModel;
+    }
+
+    public void SelectSourcePath(object sender, RoutedEventArgs routedEventArgs)
+    {
+        OpenFolderDialog dialog = new OpenFolderDialog();
+
+        // Show the dialog
+        if (dialog.ShowDialog() == true)
+        {
+            if (Directory.Exists(dialog.FolderName))
+            {
+                _sourcePath = new DirectoryInfo(dialog.FolderName);
+            }
+            else
+            {
+                Console.WriteLine(I18N.GetLocalizedMessage("invalidPath"));
+            }
+        }
+    }
+
+    public void SelectTargetPath(object sender, RoutedEventArgs routedEventArgs)
+    {
+        OpenFolderDialog dialog = new OpenFolderDialog();
+
+        // Show the dialog
+        if (dialog.ShowDialog() == true)
+        {
+            if (Directory.Exists(dialog.FolderName))
+            {
+                _targetPath = new DirectoryInfo(dialog.FolderName);
+            }
+            else
+            {
+                Console.WriteLine(I18N.GetLocalizedMessage("invalidPath"));
+            }
+        }
     }
 
     public void SendJobData(object sender, RoutedEventArgs routedEventArgs)
     {
+        //TODO: mandatory fields + add encryptor type (not mandatory) and encryptor key (mandatory if type is not null)
+        jobListViewModel.AddJob(
+            _sourcePath,
+            _targetPath,
+            RecursiveInput.IsChecked ?? false,
+            DifferentialInput.IsChecked ?? false,
+            NameInput.Text
+        );
 
+        Close();
     }
 }
 
