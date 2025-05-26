0cf035d5d87c79ff93bddaada164692554c6ebc8
kellian <kellian.bechtel@proton.me>
2025-05-26 15:06:00 +0200

    Formatting fixes

--- BackupUtil.Core/Command/ProgramFilter.cs
+++ BackupUtil.Core/Command/ProgramFilter.cs
@@ -6,13 +6,17 @@ public class ProgramFilter(List<string> bannedPrograms)
 {
     public List<string> BannedPrograms { get; set; } = bannedPrograms;
 
+    public static HashSet<string> RunningProcesses => Process.GetProcesses()
+        .Select(p => p.ProcessName.ToLower())
+        .ToHashSet();
+
     /// <summary>
     ///     Check if a banned program is running
     /// </summary>
     /// <exception cref="BannedProgramRunningException">a banned program is running</exception>
     public void CheckForBannedPrograms()
     {
-        string? bannedProgramName = BannedPrograms.Find((x) => RunningProcesses.Contains(x));
+        string? bannedProgramName = BannedPrograms.Find(x => RunningProcesses.Contains(x));
 
         if (bannedProgramName is null)
         {
@@ -21,10 +25,6 @@ public class ProgramFilter(List<string> bannedPrograms)
 
         throw new BannedProgramRunningException("errorBannedProgramRunning", bannedProgramName);
     }
-
-    public static HashSet<string> RunningProcesses => Process.GetProcesses()
-        .Select(p => p.ProcessName.ToLower())
-        .ToHashSet();
 }
 
 public class BannedProgramRunningException : Exception
