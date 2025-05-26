b52f3946c8c85d83e27e4452ab601d56b1437fb4
kellian <kellian.bechtel@proton.me>
2025-05-26 14:40:27 +0200

    Added ProgramFilterStore + Added i18n strings

---
+++ BackupUtil.ViewModel/Store/ProgramFilterStore.cs
@@ -0,0 +1,25 @@
+﻿using BackupUtil.Core.Command;
+
+namespace BackupUtil.ViewModel.Store;
+
+public class ProgramFilterStore(ProgramFilter programFilter)
+{
+    private ProgramFilter _programFilter = programFilter;
+
+    public ProgramFilter ProgramFilter
+    {
+        get => _programFilter;
+        set
+        {
+            _programFilter = value;
+            OnProgramFilterChanged();
+        }
+    }
+
+    public event Action? ProgramFilterChanged;
+
+    private void OnProgramFilterChanged()
+    {
+        ProgramFilterChanged?.Invoke();
+    }
+}
