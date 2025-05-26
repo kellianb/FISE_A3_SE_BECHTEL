ac746dda11a0ff326d723a82ef6a42410a579174
kellian <kellian.bechtel@proton.me>
2025-05-26 14:09:26 +0200

    Added program filter class to prevent execution of jobs when a banned program is running

---
+++ BackupUtil.Core/Command/ProgramFilter.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace BackupUtil.Core.Command;
+
+public class ProgramFilter(List<string> bannedPrograms)
+{
+    public List<string> BannedPrograms { get; set; } = bannedPrograms;
+
+    /// <summary>
+    ///     Check if a banned program is running
+    /// </summary>
+    /// <exception cref="BannedProgramRunningException">a banned program is running</exception>
+    public void CheckForBannedPrograms()
+    {
+        string? bannedProgramName = BannedPrograms.Find((x) => RunningProcesses.Contains(x));
+
+        if (bannedProgramName is null)
+        {
+            return;
+        }
+
+        throw new BannedProgramRunningException("errorBannedProgramRunning", bannedProgramName);
+    }
+
+    public static HashSet<string> RunningProcesses => Process.GetProcesses()
+        .Select(p => p.ProcessName.ToLower())
+        .ToHashSet();
+}
+
+public class BannedProgramRunningException : Exception
+{
+    internal BannedProgramRunningException(string message, string bannedProgramName) : base(message)
+    {
+        BannedProgramName = bannedProgramName;
+    }
+
+    public string BannedProgramName { get; }
+}
