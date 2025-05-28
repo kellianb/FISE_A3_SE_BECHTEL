05689f2148222deb2d8ec3e28668ed1922c6efd5
kellian <kellian.bechtel@proton.me>
2025-05-28 11:55:15 +0200

    Added an initial version of BackupCommandStore

---
+++ BackupUtil.ViewModel/Store/BackupCommandStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BackupUtil.Core.Command;
+
+namespace BackupUtil.ViewModel.Store;
+
+public class BackupCommandStore : INotifyPropertyChanged
+{
+    #region BackupCommands
+
+    public ObservableCollection<BackupCommand> BackupCommands { get; } = [];
+
+    #endregion
+
+    #region PropertyChanged event handler
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    #endregion
+}
