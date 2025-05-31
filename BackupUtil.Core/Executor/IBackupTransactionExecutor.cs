2c4705ba44bd4953db0f753f1daeb7b926a0f225
kellian <kellian.bechtel@proton.me>
2025-05-31 12:11:46 +0200

    Switched the callback parameters to delegates

--- BackupUtil.Core/Executor/IBackupTransactionExecutor.cs
+++ BackupUtil.Core/Executor/IBackupTransactionExecutor.cs
@@ -4,10 +4,13 @@ namespace BackupUtil.Core.Executor;
 
 internal interface IBackupTransactionExecutor
 {
+    delegate void CancelCallback();
+    delegate void ProgressCallback(string message, CurrentOperationType? operationType);
+
     void Execute(BackupTransaction transaction);
 
     public Task ExecuteAsync(BackupTransaction transaction,
-        Action shouldCancel,
-        Action<string, CurrentOperationType?> updateProgress,
+        CancelCallback shouldCancel,
+        ProgressCallback updateProgress,
         CancellationToken cancellationToken = default);
 }
